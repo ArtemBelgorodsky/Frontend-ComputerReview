7ed2a71f4308b73c228e84b233d56c47c175f118
ArtemBelgorodky <artem.belgorodkiy@yandex.ru>
2022-10-24 22:06:04 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Layout } from "antd";
+import React from "react";
+import "antd/dist/antd.css";
+import "./style.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const { Header, Footer, Sider, Content } = Layout;
 
+const App = () => (
+  <>
+    <Layout>
+      <Header className="header">
+        <div className="header_icon enter"></div>
+        <div className="header_icon register"></div>
+      </Header>
+      <div className="logo_wrapper">
+        <h1 className="logo">computeRReview</h1>
+      </div>
+      <Layout className="main_layout">
+        <Sider span={4}>Sider</Sider>
+        <Content span={16}>Content</Content>
+        <Sider span={4}>Sider</Sider>
+      </Layout>
+      <Footer>Footer</Footer>
+    </Layout>
+  </>
+);
 export default App;
