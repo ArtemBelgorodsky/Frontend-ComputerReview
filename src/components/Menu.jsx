b3c2053082b4d1ccef543f17ecbeb81424113524
ArtemBelgorodky <artem.belgorodkiy@yandex.ru>
2022-10-26 21:09:30 +0300

    сделал меню

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,9 +4,11 @@ import { useState, useRef, useEffect } from "react";
 export default function Menu() {
   const [active, setActive] = useState(0);
   const [position, setPosition] = useState(0);
+  const [visible, setVisible] = useState(false);
   const activeItem = (index, pos) => {
     setActive(index);
-    setPosition(pos);
+    pos && setPosition(pos);
+    visible && setVisible(!visible);
   };
 
   const MenuList = [
@@ -17,7 +19,9 @@ export default function Menu() {
       submenu2: "Комплектующие к ПК",
       submenu3: "Смартфоны",
     },
-    { title: "Аналитика" },
+    {
+      title: "Аналитика",
+    },
     { title: "Наш магазин" },
   ];
 
@@ -30,29 +34,46 @@ export default function Menu() {
   useEffect(() => {
     setPosition(handleOffsetLeft.current.offsetLeft);
     window.addEventListener("resize", positionHandler);
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
       window.removeEventListener("resize", positionHandler);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
+
+  function handleClickOutside(event) {
+    if (
+      handleOffsetLeft.current &&
+      !handleOffsetLeft.current.contains(event.target)
+    ) {
+      setVisible(!visible);
+    }
+  }
 
   return (
     <div className="site_menu_wrapper">
       <div className="site_menu">
-        {MenuList.map(
-          (elem, index) => (
-            console.log(elem),
-            (
-              <a
-                className={index == active ? "active item" : "item"}
-                key={index}
-                ref={index == active ? handleOffsetLeft : null}
-                onClick={(event) => activeItem(index, event.target.offsetLeft)}
-              >
-                {elem}
-              </a>
-            )
-          )
-        )}
+        {MenuList.map((elem, index) => (
+          <a
+            className={index == active ? "active item" : "item"}
+            key={index}
+            ref={index == active ? handleOffsetLeft : null}
+            onClick={(event) => activeItem(index, event.target.offsetLeft)}
+          >
+            {elem.title}
+            <div
+              className={
+                active == index && !visible ? "push_menu_visble" : "push_menu"
+              }
+            >
+              {Object.keys(elem).map((sub) =>
+                sub == "title" ? null : (
+                  <a className="push_menu_item">{elem[sub]}</a>
+                )
+              )}
+            </div>
+          </a>
+        ))}
         <div className="selection" style={{ left: position }}></div>
       </div>
     </div>
---
+++ src/components/PushUpMenu.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PushUpMenu() {
+  return <div className="push_menu">PushUpMenu</div>;
+}
